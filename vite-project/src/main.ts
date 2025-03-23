8ef6478eb1280b93912e84249a75209671261c47
ice breaker <1324318532@qq.com>
2025-03-24 00:25:57 +0800

    das

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="xx">
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
+import fs from 'fs/promises'
+debugger
 const r = r => {
     return path.relative(__dirname, r)
 }
 export default defineConfig(
+
     {
+        logLevel: 'silent',
         build: {
-            minify: false
+            minify: false,
+
         },
         plugins: [
             {
@@ -19,11 +24,22 @@ export default defineConfig(
                 async transform(code, id) {
                     console.log('transform', r(id))
                     if (r(id).includes('src/style.css')) {
-                        const resolveId = await this.resolve(path.resolve(__dirname, 'src/other.css'), path.resolve(__dirname, 'src/main.ts'))
+                        const resolveId = await this.resolve(path.resolve(__dirname, 'src/other.css'), path.resolve(__dirname, 'src/style.css'))
                         if (resolveId) {
-                            await this.load(resolveId)
+                            const otherCss = await this.load(resolveId);
+                            console.log('otherCss', otherCss.code)
+                            const code = await fs.readFile(path.resolve(__dirname, 'src/other.css'), 'utf-8')
+                            console.log('code', code)
+                            if (otherCss) {
+                                // 合并 src/style.css 和 src/other.css 的内容
+                                const combinedCss = `${code}\n${otherCss.code}`;
+                                return combinedCss;
+                            }
                         }
                     }
+                    //  else if (r(id).includes('src/other.css')) {
+                    //     return await fs.readFile(path.resolve(__dirname, 'src/other.css'), 'utf-8')
+                    // }
                 },
                 load(id, options) {
                     console.log('load', r(id))
